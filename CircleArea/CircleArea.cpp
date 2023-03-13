b04e4ba2a6d40ecf86064ed54983b6cdc254df65
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2023-03-13 17:12:16 -0500

    Add files via upload

---
+++ CircleArea/CircleArea.cpp
@@ -0,0 +1,18 @@
+/*
+Circle Area
+Pawelski
+3/13/2023
+C++
+*/
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    // declare a constant called pi with the value 3.14159
+    // declare a variable called radius with the value 4
+    double area = pi * pow(radius, 2);
+    std::cout << "Area of the circle: " << area;
+    return 0;
+}
---
+++ Numbers/Numbers.cpp
@@ -0,0 +1,16 @@
+/*
+Numbers
+Pawelski
+3/13/2023
+C++
+*/
+
+#include <iostream>
+
+int main()
+{
+    int digit1 = 1;
+    int digit2 = 6;
+    std::cout << digit1 << digit2 << "\n";
+    return 0;
+}
